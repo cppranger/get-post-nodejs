cb7c3f081d926d0d1d7bd9a628cb2a9d07fa853c
cppranger <keksovska@gmail.com>
2021-05-01 18:48:03 +0300

    final

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 const http = require('http');
 const url = require("url");
+const { parse } = require('querystring');
 
 http.createServer((request, response) => {
     console.log('server work');
-    let urlRequest = url.parse(request.url, true);
-    console.log(urlRequest.query.test);
-    if (urlRequest.query.test === "hello") {
-        response.end('hello, world!')
+    if (request.method === 'GET') {
+        let urlRequest = url.parse(request.url, true);
+        console.log(urlRequest.query.test);
+        if (urlRequest.query.test === "hello") {
+            response.end('hello, world!')
+        }
+        else {
+            response.end('not hello')
+        }
     }
-    else {
-        response.end('not hello')
+    else if (request.method === 'POST') {
+        let body = '';
+        request.on('data', chunk => {
+            body += chunk.toString();
+        });
+        request.on('end', () => {
+            console.log(body);
+            let params = parse(body);
+            console.log(params);
+            response.end('ok');
+        })
     }
 }).listen(3000);
